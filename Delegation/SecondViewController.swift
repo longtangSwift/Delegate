969df702e68573f6a6c96039eef8b542be1c110d
Chin T <tang91766@yahoo.com>
2015-07-02 23:51:53 -0700

    initial

---
+++ Delegation/SecondViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SecondViewController.swift
+//  Delegation
+//
+//  Created by CT MacBook Pro on 7/2/15.
+//  Copyright © 2015 CT MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+protocol DataEnteredDelegate: class{
+    func userDidEnterInformation(info: String)
+}
+
+class SecondViewController: UIViewController {
+
+    
+    var delegate: DataEnteredDelegate? = nil
+    
+    
+    @IBOutlet weak var dataTextField: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendData(sender: AnyObject) {
+        if delegate != nil {
+            let information: String = dataTextField.text
+            delegate!.userDidEnterInformation(information)
+            self.navigationController?.popViewControllerAnimated(true)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Delegation/ViewController.swift
+++ Delegation/ViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, DataEnteredDelegate {
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    func userDidEnterInformation(info: String) {
+        dataLabel.text = info
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showSecondVC"{
+            let secondVC: SecondViewController = segue.destinationViewController as! SecondViewController
+            secondVC.delegate = self //because we have adapted to its protocl.
+            
+        }
     }
-
-
 }
-
