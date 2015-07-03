cc9a70304488fbb42d31282d1fef6231a3f72d0f
Chin T <tang91766@yahoo.com>
2015-07-03 00:31:29 -0700

    intentionally broken

--- Delegation/ViewController.swift
+++ Delegation/ViewController.swift
@@ -15,18 +15,7 @@ class ViewController: UIViewController, DataEnteredDelegate {
     
     func userDidEnterInformation(info: String) {
         dataLabel.text = info
-    }
-    func getSomeXtraInf(randNumStr: String) {
-        extraInfo.text = "bonus " + randNumStr
-    }
-
-    func getExtraInfo(inf: String){
-        extraInfo.text = inf
-    }
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showSecondVC"{
-            let secondVC: SecondViewController = segue.destinationViewController as! SecondViewController
-            secondVC.delegate = self //because we have adapted to its protocl. we call it self
+    }gC.delegate = self //because we have adapted to its protocl. we call it self
             
         }
     }
